3cb0d25548844f58cc1757f0edc69837ce0aa692
ivtemplates <86544399+ivtemplates@users.noreply.github.com>
2021-08-16 11:33:28 +0100

    Create notif-bar.js

---
+++ notif-bar.js
@@ -0,0 +1,12 @@
+var e = document.createElement("script");
+e.type = "text/javascript", e.src = "https://cdn.statically.io/gh/ivtemplates/Fb-blogger-coments/main/powr-io.js", e.async = "true";
+var t = document.getElementsByTagName("script")[0];
+t.parentNode.insertBefore(e, t)
+}();
+!function() {
+var qq = document.createElement('div');
+qq.id = 'd34778be_1574755911';
+qq.className = 'powr-notification-bar';
+document.body.appendChild(qq);
+//document.getElementsByTagName("BODY")[0].appendChild(qq);
+}();
